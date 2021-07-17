ce12d8c76d2fc457164bfbddeb40622ddeb2e859
André Schröder <andre.schroedr@gmail.com>
2021-07-17 16:08:58 +0200

    [feat] Add a CI to build and push the images

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Build and optionally push a Docker image.
+
+set -Eeuo pipefail
+
+args=$(getopt -o 'i:p:' --long 'image:,image-name:,push:' -- "$@")
+eval "set -- $args"
+
+while true; do
+  case $1 in
+  -p | --push)
+    registry=$2
+    shift 2
+    ;;
+  -i | --image | --image-name)
+    image_path=$2
+    shift 2
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *)
+    exit 1
+    ;;
+  esac
+done
+
+trap 'echo >&2 $0: line $LINENO: $BASH_COMMAND: Failed with status $?' ERR
+
+image_name=$(basename "$(realpath "$image_path")")
+
+if [[ -v registry ]]; then
+  # e.g. in spotify/ we need --no-cache because the Spotify app might
+  # update without the parent image updating
+  docker build "$image_path" --pull --no-cache --tag "$registry/$image_name"
+  docker push "$registry/$image_name"
+else
+  docker build "$image_path" --pull --tag "$image_name"
+fi
